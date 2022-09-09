333585a99c286db9e280c5ab54bb18461286bc80
Parthiv-M <parthiv.menon@learner.manipal.edu>
2022-09-09 15:54:56 +0530

    styling and validation updates

--- client/src/App.js
+++ client/src/App.js
@@ -4,25 +4,38 @@ import { useState } from "react";
 const App = () => {
 
   const [title, setTitle] = useState("");
+  const [loading, setLoading] = useState(false);
 
   // function to query the API to get combined chapters
   const getChapterData = async () => {
+    setLoading(true);
+    document.querySelector(".display-html").innerHTML = "";
+    if (title === "") {
+      alert("Please enter a title");
+      return;
+    }
     try {
       const resp = await axios.get(`/api/getchapters/${title}`);
-      if(resp.status === 200) {
+      if (resp.status === 200) {
         // get the new combined html document
+        setLoading(false);
         document.querySelector(".display-html").innerHTML = resp.data;
       }
     } catch (error) {
+      setLoading(false);
       window.alert(error.response.data.message)
     }
   }
   return (
     <div className="wrapper">
       <div className="user-input">
-        <input placeholder="Enter title of work" onChange={(e) => setTitle(e.target.value)} />
-        <button onClick={getChapterData}>Search</button>
+      <h3>WikiSource Featured Text Search</h3>
+        <div className="input-area">
+          <input placeholder="Enter title of work" onChange={(e) => setTitle(e.target.value)} />
+          <button onClick={getChapterData}>Search</button>
+        </div>
       </div>
+      {loading && <p className="loading">Loading all chapters...</p>}
       <div className="display-html"></div>
     </div>
   );
